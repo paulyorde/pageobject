0c2083098e0cca52380ee294e499a08b9a89cfdf
paulyorde <paulyorde26@gmail.com>
2017-07-26 09:45:31 -0400

    dynamitized parameters

--- features/Amazon_Feature.feature
+++ features/Amazon_Feature.feature
@@ -1,5 +1,10 @@
 Feature: Amazon Search functionality
   Scenario: Users can search for a specific item on Amazon
-    Given a user goes to Amzon
+    Given a user goes to Amazon
     When they search for pens
     Then amazon should return results for pens
+
+  Scenario: Users can search for any item on Amazon
+    Given using Amazon
+    When  search for "pens"
+    Then amazon returns results for "pens"
--- features/step_definitions/amazon_search.rb
+++ features/step_definitions/amazon_search.rb
@@ -1,4 +1,4 @@
-Given(/^a user goes to Amzon$/) do
+Given(/^a user goes to Amazon$/) do
   visit_page Search
 end
 
@@ -20,3 +20,26 @@ Then(/^amazon should return results for pens$/) do
   end
 
 end
+
+Given(/^using Amazon$/) do
+  visit_page Search
+end
+
+When(/^search for "([^"]*)"$/) do |arg|
+  on_page Search do |page|
+    page.search_for_items=arg
+    # how to set parameter for return
+    #page.send
+    page.submit
+    sleep 2
+    end
+end
+
+
+Then(/^amazon returns results for "([^"]*)"$/) do |arg1|
+    on_page SearchResult do |page|
+    expect(page.response).to include arg1
+  end
+end
+
+
